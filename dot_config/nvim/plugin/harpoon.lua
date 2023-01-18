ccf1911258ab2b54fccf22f3b5ae8d26ae38eb0e
g3offrey <11151445+g3offrey@users.noreply.github.com>
2023-01-18 22:56:53 +0100

    improve harpoon shortcuts

--- dot_config/nvim/plugin/harpoon.lua
+++ dot_config/nvim/plugin/harpoon.lua
@@ -1,5 +1,8 @@
 local harpoon_mark = require("harpoon.mark")
 local harpoon_ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>hh", harpoon_mark.add_file)
+vim.keymap.set("n", "<leader>hd", harpoon_mark.clear_all)
+vim.keymap.set("n", "<leader>hh", harpoon_mark.toggle_file)
 vim.keymap.set("n", "<leader>hl", harpoon_ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>hj", harpoon_ui.nav_next)
+vim.keymap.set("n", "<leader>hk", harpoon_ui.nav_prev)
