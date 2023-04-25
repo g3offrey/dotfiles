c21439d635596e42930ab47ed05b4f56b3836887
g3offrey <11151445+g3offrey@users.noreply.github.com>
2023-04-25 17:41:44 +0200

    remove useless keymap

--- dot_config/nvim/after/plugin/yanky.lua
+++ dot_config/nvim/after/plugin/yanky.lua
@@ -4,8 +4,6 @@ yanky.setup {}
 
 vim.keymap.set({"n", "x"}, "p", "<Plug>(YankyPutAfter)")
 vim.keymap.set({"n", "x"}, "P", "<Plug>(YankyPutBefore)")
-vim.keymap.set({"n", "x"}, "gp", "<Plug>(YankyGPutAfter)")
-vim.keymap.set({"n", "x"}, "gP", "<Plug>(YankyGPutBefore)")
 
 vim.keymap.set("n", "<c-n>", "<Plug>(YankyCycleForward)")
 vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleBackward)")
