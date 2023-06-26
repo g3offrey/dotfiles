9097d0fb11d228f059617c5c676097af37866f5a
g3offrey <11151445+g3offrey@users.noreply.github.com>
2023-06-26 09:36:12 +0200

    replace nvim-tree by oil

--- dot_config/nvim/lua/base.lua
+++ dot_config/nvim/lua/base.lua
@@ -1,6 +1,5 @@
 vim.g.mapleader = ","
 
--- required for nvim-tree
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -40,23 +40,44 @@ return {
         end
     },
     {
-        "nvim-tree/nvim-tree.lua",
+        "stevearc/oil.nvim",
+        opts = {
+            float = {
+                border = "single"
+            }
+        },
         dependencies = {"nvim-tree/nvim-web-devicons"},
-        config = function()
-            local nvim_tree = require("nvim-tree")
-            local nvim_tree_api = require("nvim-tree.api")
-
-            nvim_tree.setup {
-                update_focused_file = {
-                    enable = true
-                },
-                hijack_directories = {
-                    enable = true,
-                    auto_open = false
+        keys = {
+            {
+                "<leader>n",
+                mode = {"n"},
+                function()
+                    require("oil").toggle_float()
+                end,
+                {
+                    desc = "Toggle file tree"
                 }
             }
-
-            vim.keymap.set("n", "<leader>n", nvim_tree_api.tree.toggle, {desc = "Toggle file tree"})
-        end
+        }
     }
+    -- {
+    --     "nvim-tree/nvim-tree.lua",
+    --     dependencies = {"nvim-tree/nvim-web-devicons"},
+    --     config = function()
+    --         local nvim_tree = require("nvim-tree")
+    --         local nvim_tree_api = require("nvim-tree.api")
+    --
+    --         nvim_tree.setup {
+    --             update_focused_file = {
+    --                 enable = true
+    --             },
+    --             hijack_directories = {
+    --                 enable = true,
+    --                 auto_open = false
+    --             }
+    --         }
+    --
+    --         vim.keymap.set("n", "<leader>n", nvim_tree_api.tree.toggle, {desc = "Toggle file tree"})
+    --     end
+    -- }
 }
