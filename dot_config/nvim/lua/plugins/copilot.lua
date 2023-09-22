56642ca3d7ad9168e5bcb454f21a536894ed719c
g3offrey <11151445+g3offrey@users.noreply.github.com>
2023-09-22 14:31:55 +0200

    revert copilot lazy

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,36 +1,18 @@
 return {
     {
-        lazy = false,
         "github/copilot.vim",
-        keys = {
-            {
+        config = function()
+            vim.keymap.set(
+                "i",
                 "<leader><tab>",
-                mode = {"i"},
                 'copilot#Accept("")',
                 {expr = true, replace_keycodes = false, desc = "Accept copilot suggestion"}
-            },
-            {
-                "<C-j>",
-                mode = {"i"},
-                "<Plug>(copilot-next)"
-            },
-            {
-                "<C-k>",
-                mode = {"i"},
-                "<Plug>(copilot-previous)"
-            },
-            {
-                "<C-d>",
-                mode = {"i"},
-                "<Plug>(copilot-dismiss)"
-            },
-            {
-                "<C-s>",
-                mode = {"i"},
-                "<Plug>(copilot-suggest)"
-            }
-        },
-        config = function()
+            )
+            vim.keymap.set("i", "<C-j>", "<Plug>(copilot-next)")
+            vim.keymap.set("i", "<C-k>", "<Plug>(copilot-previous)")
+            vim.keymap.set("i", "<C-d>", "<Plug>(copilot-dismiss)")
+            vim.keymap.set("i", "<C-s>", "<Plug>(copilot-suggest)")
+
             vim.g.copilot_no_tab_map = true
         end
     }
