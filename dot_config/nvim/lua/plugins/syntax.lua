53f6f4eda7c4b146a904b2d053bc65b7e038632c
g3offrey <11151445+g3offrey@users.noreply.github.com>
2023-09-22 10:55:08 +0200

    fix ts

--- dot_config/nvim/lua/plugins/syntax.lua
+++ dot_config/nvim/lua/plugins/syntax.lua
@@ -6,58 +6,64 @@ return {
             "nvim-treesitter/nvim-treesitter-textobjects",
             "nvim-treesitter/nvim-treesitter-context"
         },
-        opts = {
-            ensure_installed = "all",
-            highlight = {
-                enable = true
-            },
-            indent = {
-                enable = true
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<S-Right>",
-                    node_incremental = "<S-Right>",
-                    scope_incremental = "<S-Up>",
-                    node_decremental = "<S-Left>"
-                }
-            },
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ["aa"] = "@parameter.outer",
-                        ["ia"] = "@parameter.inner",
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ac"] = "@class.outer",
-                        ["ic"] = "@class.inner"
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+
+            configs.setup(
+                {
+                    ensure_installed = "all",
+                    highlight = {
+                        enable = true
+                    },
+                    indent = {
+                        enable = true
+                    },
+                    incremental_selection = {
+                        enable = true,
+                        keymaps = {
+                            init_selection = "<S-Right>",
+                            node_incremental = "<S-Right>",
+                            scope_incremental = "<S-Up>",
+                            node_decremental = "<S-Left>"
+                        }
+                    },
+                    textobjects = {
+                        select = {
+                            enable = true,
+                            lookahead = true,
+                            keymaps = {
+                                ["aa"] = "@parameter.outer",
+                                ["ia"] = "@parameter.inner",
+                                ["af"] = "@function.outer",
+                                ["if"] = "@function.inner",
+                                ["ac"] = "@class.outer",
+                                ["ic"] = "@class.inner"
+                            }
+                        },
+                        move = {
+                            enable = true,
+                            set_jumps = true,
+                            goto_next_start = {
+                                [")m"] = "@function.outer",
+                                ["))"] = "@class.outer"
+                            },
+                            goto_next_end = {
+                                [")M"] = "@function.outer",
+                                [")("] = "@class.outer"
+                            },
+                            goto_previous_start = {
+                                ["(m"] = "@function.outer",
+                                ["(("] = "@class.outer"
+                            },
+                            goto_previous_end = {
+                                ["(M"] = "@function.outer",
+                                ["()"] = "@class.outer"
+                            }
+                        }
                     }
-                },
-                move = {
-                    enable = true,
-                    set_jumps = true,
-                    goto_next_start = {
-                        [")m"] = "@function.outer",
-                        ["))"] = "@class.outer"
-                    },
-                    goto_next_end = {
-                        [")M"] = "@function.outer",
-                        [")("] = "@class.outer"
-                    },
-                    goto_previous_start = {
-                        ["(m"] = "@function.outer",
-                        ["(("] = "@class.outer"
-                    },
-                    goto_previous_end = {
-                        ["(M"] = "@function.outer",
-                        ["()"] = "@class.outer"
-                    }
                 }
-            }
-        }
+            )
+        end
     },
     {"windwp/nvim-autopairs", dependencies = "nvim-treesitter/nvim-treesitter", opts = {}},
     {"windwp/nvim-ts-autotag", dependencies = "nvim-treesitter/nvim-treesitter", opts = {}}
