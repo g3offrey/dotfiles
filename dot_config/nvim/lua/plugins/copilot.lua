5ef034bebe88b6acef80d36b60c67570b5165fad
g3offrey <11151445+g3offrey@users.noreply.github.com>
2023-10-02 16:22:04 +0200

    change copilot mapping

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -2,18 +2,18 @@ return {
     {
         "github/copilot.vim",
         config = function()
+            vim.g.copilot_no_tab_map = true
+
             vim.keymap.set(
                 "i",
-                "<leader><tab>",
+                "<C-j>",
                 'copilot#Accept("")',
                 {expr = true, replace_keycodes = false, desc = "Accept copilot suggestion"}
             )
-            vim.keymap.set("i", "<C-j>", "<Plug>(copilot-next)")
-            vim.keymap.set("i", "<C-k>", "<Plug>(copilot-previous)")
+            vim.keymap.set("i", "<C-l>", "<Plug>(copilot-next)")
+            vim.keymap.set("i", "<C-h>", "<Plug>(copilot-previous)")
             vim.keymap.set("i", "<C-d>", "<Plug>(copilot-dismiss)")
-            vim.keymap.set("i", "<C-s>", "<Plug>(copilot-suggest)")
-
-            vim.g.copilot_no_tab_map = true
+            vim.keymap.set("i", "<C-k>", "<Plug>(copilot-suggest)")
         end
     }
 }
